b51b97ca4bbdb18476de7c4141535536e3d7464b
Hee-Soo <hee-soo.choi@loria.fr>
2023-06-15 01:06:20 +0200

    Update sweep files

--- sweep_trim0.1.sh
+++ sweep_trim0.1.sh
@@ -1,11 +1,11 @@
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.01 -batch 128 -gcn_layer 1 -name tr0.1_lr1e1_bs128_gcn1
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.005 -batch 128 -gcn_layer 1 -name tr0.1_lr5e2_bs128_gcn1
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0005 -batch 128 -gcn_layer 1 -name tr0.1_lr5e3_bs128_gcn1
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.001 -batch 128 -gcn_layer 1 -name tr0.1_lr1e2_bs128_gcn1
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0001 -batch 128 -gcn_layer 1 -name tr0.1_lr1e3_bs128_gcn1
-
-
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.01 -batch 32 -gcn_layer 1 -name tr0.1_lr1e1_bs32_gcn1
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.01 -batch 128 -gcn_layer 1 -name tr0.1_lr1e1_bs128_gcn1
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.005 -batch 128 -gcn_layer 1 -name tr0.1_lr5e2_bs128_gcn1
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0005 -batch 128 -gcn_layer 1 -name tr0.1_lr5e3_bs128_gcn1
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.001 -batch 128 -gcn_layer 1 -name tr0.1_lr1e2_bs128_gcn1
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0001 -batch 128 -gcn_layer 1 -name tr0.1_lr1e3_bs128_gcn1
+#
+#
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.01 -batch 32 -gcn_layer 1 -name tr0.1_lr1e1_bs32_gcn1
 python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.005 -batch 32 -gcn_layer 1 -name tr0.1_lr5e2_bs32_gcn1
 python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0005 -batch 32 -gcn_layer 1 -name tr0.1_lr5e3_bs32_gcn1
 python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.001 -batch 32 -gcn_layer 1 -name tr0.1_lr1e2_bs32_gcn1

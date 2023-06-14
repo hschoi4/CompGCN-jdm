b50279495033c85f990c38d1b2763a66cc55460a
Hee-Soo <hee-soo.choi@loria.fr>
2023-06-14 16:06:13 +0200

    Add sweep file

---
+++ sweep.sh
@@ -0,0 +1,41 @@
+# python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.01 -batch 128 -gcn_layer 1 -name lr1e1_bs128_gcn1
+# python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.005 -batch 128 -gcn_layer 1 -name lr5e2_bs128_gcn1
+# python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0005 -batch 128 -gcn_layer 1 -name lr5e3_bs128_gcn1
+
+# TODO:
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.001 -batch 128 -gcn_layer 1 -name lr1e2_bs128_gcn1
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0001 -batch 128 -gcn_layer 1 -name lr1e3_bs128_gcn1
+
+
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.01 -batch 32 -gcn_layer 1 -name lr1e1_bs32_gcn1
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.005 -batch 32 -gcn_layer 1 -name lr5e2_bs32_gcn1
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0005 -batch 32 -gcn_layer 1 -name lr5e3_bs32_gcn1
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.001 -batch 32 -gcn_layer 1 -name lr1e2_bs32_gcn1
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0001 -batch 32 -gcn_layer 1 -name lr1e3_bs32_gcn1
+
+
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.01 -batch 512 -gcn_layer 1 -name lr1e1_bs512_gcn1
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.005 -batch 512 -gcn_layer 1 -name lr5e2_bs512_gcn1
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0005 -batch 512 -gcn_layer 1 -name lr5e3_bs512_gcn1
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.001 -batch 512 -gcn_layer 1 -name lr1e2_bs512_gcn1
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0001 -batch 512 -gcn_layer 1 -name lr1e3_bs512_gcn1
+
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.01 -batch 128 -gcn_layer 2 -name lr1e1_bs128_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.005 -batch 128 -gcn_layer 2 -name lr5e2_bs128_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0005 -batch 128 -gcn_layer 2 -name lr5e3_bs128_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.001 -batch 128 -gcn_layer 2 -name lr1e2_bs128_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0001 -batch 128 -gcn_layer 2 -name lr1e3_bs128_gcn2
+
+
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.01 -batch 32 -gcn_layer 2 -name lr1e1_bs32_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.005 -batch 32 -gcn_layer 2 -name lr5e2_bs32_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0005 -batch 32 -gcn_layer 2 -name lr5e3_bs32_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.001 -batch 32 -gcn_layer 2 -name lr1e2_bs32_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0001 -batch 32 -gcn_layer 2 -name lr1e3_bs32_gcn2
+
+
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.01 -batch 512 -gcn_layer 2 -name lr1e1_bs512_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.005 -batch 512 -gcn_layer 2 -name lr5e2_bs512_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0005 -batch 512 -gcn_layer 2 -name lr5e3_bs512_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.001 -batch 512 -gcn_layer 2 -name lr1e2_bs512_gcn2
+python3 run.py -epoch 100 -trim True -gpu 0 -use_wandb True -lr 0.0001 -batch 512 -gcn_layer 2 -name lr1e3_bs512_gcn2

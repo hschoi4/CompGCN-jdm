5ed264b671ee7b33aa9b0d8528af06af82509f23
Hee-Soo <hee-soo.choi@loria.fr>
2023-06-24 14:32:23 +0200

    Update sweep file

--- sweep_trim0.1.sh
+++ sweep_trim0.1.sh
@@ -17,17 +17,17 @@
 # python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.001 -batch 512 -gcn_layer 1 -name tr0.1_lr1e2_bs512_gcn1
 # python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0001 -batch 512 -gcn_layer 1 -name tr0.1_lr1e3_bs512_gcn1
 
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.01 -batch 128 -gcn_layer 2 -name tr0.1_lr1e1_bs128_gcn2
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.005 -batch 128 -gcn_layer 2 -name tr0.1_lr5e2_bs128_gcn2
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0005 -batch 128 -gcn_layer 2 -name tr0.1_lr5e3_bs128_gcn2
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.001 -batch 128 -gcn_layer 2 -name tr0.1_lr1e2_bs128_gcn2
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0001 -batch 128 -gcn_layer 2 -name tr0.1_lr1e3_bs128_gcn2
-
-
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.01 -batch 32 -gcn_layer 2 -name tr0.1_lr1e1_bs32_gcn2
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.005 -batch 32 -gcn_layer 2 -name tr0.1_lr5e2_bs32_gcn2
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0005 -batch 32 -gcn_layer 2 -name tr0.1_lr5e3_bs32_gcn2
-python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.001 -batch 32 -gcn_layer 2 -name tr0.1_lr1e2_bs32_gcn2
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.01 -batch 128 -gcn_layer 2 -name tr0.1_lr1e1_bs128_gcn2
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.005 -batch 128 -gcn_layer 2 -name tr0.1_lr5e2_bs128_gcn2
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0005 -batch 128 -gcn_layer 2 -name tr0.1_lr5e3_bs128_gcn2
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.001 -batch 128 -gcn_layer 2 -name tr0.1_lr1e2_bs128_gcn2
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0001 -batch 128 -gcn_layer 2 -name tr0.1_lr1e3_bs128_gcn2
+#
+#
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.01 -batch 32 -gcn_layer 2 -name tr0.1_lr1e1_bs32_gcn2
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.005 -batch 32 -gcn_layer 2 -name tr0.1_lr5e2_bs32_gcn2
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0005 -batch 32 -gcn_layer 2 -name tr0.1_lr5e3_bs32_gcn2
+# python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.001 -batch 32 -gcn_layer 2 -name tr0.1_lr1e2_bs32_gcn2
 python3 run.py -epoch 100 -trim True -trim_ratio 0.1 -gpu 0 -use_wandb True -lr 0.0001 -batch 32 -gcn_layer 2 -name tr0.1_lr1e3_bs32_gcn2
 
 

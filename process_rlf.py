87798313035fe8c74de93191bee9a6ac58a08a6f
Hee-Soo <hee-soo.choi@loria.fr>
2023-07-06 16:28:10 +0200

    Scripts rlf preprocessing

---
+++ process_rlf.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import collections
+import dataclasses
+from typing import Set, Any
+import xml.etree.ElementTree as ET
+
+def get_entities(df_nodes, export):
+    """
+        Get label of each node and new ids
+    """
+    entities = df_nodes['lexname'].tolist()
+    list_labels = []
+
+    # Get the label and features between tags in xml format
+    for ent in entities:
+        tag = ET.fromstring('<tag>'+ent+"</tag>")
+        acc = ""
+        for field in tag:
+            acc = acc + " " + field.text
+        list_labels.append(acc)
+
+    df_nodes['label'] = list_labels
+
+    if export:
+        df_nodes['label'].to_csv('data/RLF/entities.txt', index=True, sep='\t', header=False)
+
+    return df_nodes
+
+def get_relations(df_lffam, df_cp, export):
+    """
+        Get relation types and new ids
+    """
+    df_relations = pd.concat([df_lffam, df_cp], ignore_index=True)
+
+    if export:
+        relations['name'].to_csv('data/RLF/relations.txt', index=True, sep="\t", header=False)
+
+    return df_relations
+
+def normalize_label(df, entities, relations, export):
+    """
+        Put new ids for relations and entities
+    """
+
+    ent_map = {id_node : i for i, id_node in enumerate(entities['id'].tolist())}
+    rel_map = {id_rel : i for i, id_rel in enumerate(relations['id'].tolist())}
+
+    df['t'] = df['t'].map(rel_map)
+    df['n1'] = df['n1'].map(ent_map)
+    df['n2'] = df['n2'].map(ent_map)
+
+    df = df.drop(columns='id')
+
+    if export:
+        df.to_csv('data/RLF/all_triplets.txt', index=False, sep="\t", header=True)
+
+    return df
+
+if __name__ == '__main__':
+
+    df_nodes = pd.read_csv("data/RLF/originals/01-lsnodes.csv", sep="\t")
+    df_lffam = pd.read_csv("data/RLF/originals/lffam_ids_names.csv", sep="\t")      # lexical function families edges
+    df_cp = pd.read_csv("data/RLF/originals/cp_ids_names.csv", sep="\t")            # copolysemy edges
+
+    entities = get_entities(df_nodes, False)
+    relations = get_relations(df_lffam, df_cp, False)
+
+    df_triplets = pd.read_csv("data/RLF/originals/rlf_lffam_cp.csv", sep="\t", index_col=False)
+    df_triplets_normalized = normalize_label(df_triplets, entities, relations, False)
+
+    # print(df_triplets_normalized)
---
+++ split_rlf.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import collections
+import dataclasses
+from typing import Set, Any
+import xml.etree.ElementTree as ET
+
+RLF_PATH = "data/RLF/"
+
+@dataclasses.dataclass
+class Triplets:
+    train: Any
+    valid: Any
+    test: Any
+
+    def export(self, path, status):
+        self.train.to_csv(f"{path}triplets/train_{status}.csv", index=False, sep='\t')
+        self.valid.to_csv(f"{path}/triplets/valid_{status}.csv", index=False, sep='\t')
+        self.test.to_csv(f"{path}/triplets/test_{status}.csv", index=False, sep='\t')
+
+@dataclasses.dataclass
+class Nodes:
+    train: Set = dataclasses.field(default_factory=set)
+    valid: Set = dataclasses.field(default_factory=set)
+    test: Set = dataclasses.field(default_factory=set)
+    intersection : Set = dataclasses.field(default_factory=set)
+
+def split(df_triplets, status, export, path):
+    """
+        Divide triplets into train, valid, test sets (80%, 10%, 10%)
+        Return triplets and nodes in each set
+        Status for random state
+    """
+    train, valid, test = np.split(df_triplets.sample(frac=1, random_state=status), [int(.8*len(df_triplets)), int(.9*len(df_triplets))])
+    triplets = Triplets(train, valid, test)
+
+    if export:
+        triplets.export(path, status)
+
+    nodes = Nodes()
+    nodes.train = set(train["n1"]).union(set(train["n2"]))
+    nodes.valid = set(valid["n1"]).union(set(valid["n2"]))
+    nodes.test = set(test["n1"]).union(set(test["n2"]))
+    nodes.intersection =  nodes.train.intersection(nodes.valid).intersection(nodes.test)
+
+    return triplets, nodes
+
+def filter_set(df, train):
+    """
+        Remove from test and valid set when head, rel or tail is not in train set
+    """
+
+    train_ent = set(train['n1'].tolist() + train['n2'].tolist())
+    train_rel = set(train['t'].tolist())
+
+    df = df[df['n1'].isin(train_ent)]
+    df = df[df['n2'].isin(train_ent)]
+    df = df[df['t'].isin(train_rel)]
+
+    return df
+
+
+if __name__ == '__main__':
+
+    df_nodes = pd.read_csv("data/RLF/originals/01-lsnodes.csv", sep="\t")
+    df_triplets = pd.read_csv("data/RLF/all_triplets.txt", sep="\t", index_col=False)
+
+    entities = pd.read_csv("data/RLF/entities.txt", sep="\t")
+    relations = pd.read_csv("data/RLF/relations.txt", sep="\t")
+
+    rlf, rlf_nodes = split(df_triplets, 20, True, RLF_PATH)
+
+    filter_valid = filter_set(rlf.valid, rlf.train)
+    filter_test = filter_set(rlf.test, rlf.train)
+
+    # filter_valid.to_csv("data/RLF/valid_20.txt", index=False, header=False, sep="\t")
+    # filter_test.to_csv("data/RLF/test_20.txt", index=False, header=False, sep="\t")

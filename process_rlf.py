8989c0b4069e61be4336ce0fc244e99cd490a770
Hee-Soo <hee-soo.choi@loria.fr>
2023-07-21 10:04:28 +0200

    Update scripts new data path

--- process_rlf.py
+++ process_rlf.py
@@ -43,32 +43,12 @@ def get_relations(df_lffam, df_cp, export):
 
     return df_relations
 
-def normalize_label(df, entities, relations, export):
-    """
-        Put new ids for relations and entities
-    """
-
-    ent_map = {id_node : i for i, id_node in enumerate(entities['id'].tolist())}
-    rel_map = {id_rel : i for i, id_rel in enumerate(relations['id'].tolist())}
-
-    df['t'] = df['t'].map(rel_map)
-    df['n1'] = df['n1'].map(ent_map)
-    df['n2'] = df['n2'].map(ent_map)
-
-    df = df.drop(columns='id')
-
-    if export:
-        df.to_csv('data/RLF/all_triplets.txt', index=False, sep="\t", header=True)
-
-    return df
-
 def get_default_format(df, entities, relations, export):
     """
         Put labels for relations and entities
     """
 
     ent_map = dict(zip(entities['id'].tolist(), entities['label'].tolist()))
-    print(ent_map)
     rel_map = dict(zip(relations['id'].tolist(), relations['name'].tolist()))
 
     df['t'] = df['t'].map(rel_map)
@@ -85,13 +65,12 @@ def get_default_format(df, entities, relations, export):
 
 if __name__ == '__main__':
 
-    df_nodes = pd.read_csv("data/RLF/originals/01-lsnodes.csv", sep="\t")
+    df_nodes = pd.read_csv("data/RLF/originals/01-lsnodes.csv", sep="\t")           # nodes
     df_lffam = pd.read_csv("data/RLF/originals/lffam_ids_names.csv", sep="\t")      # lexical function families edges
     df_cp = pd.read_csv("data/RLF/originals/cp_ids_names.csv", sep="\t")            # copolysemy edges
 
-    entities = get_entities(df_nodes, True)
+    entities = get_entities(df_nodes, False)
     relations = get_relations(df_lffam, df_cp, False)
 
     df_triplets = pd.read_csv("data/RLF/originals/rlf_lffam_cp.csv", sep="\t", index_col=False)
-    # df_triplets_normalized = normalize_label(df_triplets, entities, relations, False)
     df = get_default_format(df_triplets, entities, relations, True)
